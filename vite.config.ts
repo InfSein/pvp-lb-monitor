aec965371cdce31cc6ecfbcab1e7712199f8cb95
InfSein <infsein@foxmail.com>
2025-09-02 23:54:18 +0800

    fix: github action build

--- vite.config.ts
+++ vite.config.ts
@@ -4,12 +4,15 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
+const repoName = process.env.GITHUB_REPOSITORY?.split("/")[1] ?? ""
+
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
     vueDevTools(),
   ],
+  base: process.env.GITHUB_ACTIONS ? `/${repoName}/` : "/",
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
